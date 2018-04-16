94df2defff5b9bf0b20a090973e94c4e3a26ba17
Unknown <boujenah.bruno@gmail.com>
2018-04-16 18:28:48 +0200

    servlet

---
+++ Servlet/src/org/wcs/hello/SimpleHelloServlet.java
@@ -0,0 +1,25 @@
+package org.wcs.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "SimpleHelloServlet" ,urlPatterns={"/simple-hello"})
+public class SimpleHelloServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("/WEB-INF/hello-form.jsp").forward(request, response);
+
+    }
+}
